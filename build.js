4f3bef88d1a515a198dfb5322f38a60bfedf9965
Josh Graber <jgraber@renovo1.com>
2015-06-07 10:37:55 -0400

    Allow lint to be disabled since it does not yet support ES6 modules.

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ var copy = require('./copy');
 
 var runSequence = require('run-sequence');
 
-exports.config = function(gulp, debugTask, releaseTask) {
+exports.config = function(gulp, debugTask, releaseTask, useLint) {
 	lint.config(gulp);
 	clean.config(gulp);
 	copy.config(gulp);
@@ -12,18 +12,32 @@ exports.config = function(gulp, debugTask, releaseTask) {
 	gulp.task('build', ['build.debug']);
 
 	gulp.task('build.debug', function(done) {
-		runSequence('lint',
-					'clean.debug',
-					debugTask,
-					'copy.debug',
-					done);
+		if (useLint) {
+			runSequence('lint',
+						'clean.debug',
+						debugTask,
+						'copy.debug',
+						done);
+		} else {
+			runSequence('clean.debug',
+						debugTask,
+						'copy.debug',
+						done);
+		}
 	});
 
 	gulp.task('build.release', function(done) {
-		runSequence('lint',
-					'clean.release',
-					releaseTask,
-					'copy.release',
-					done);
+		if (useLint) {
+			runSequence('lint',
+						'clean.release',
+						releaseTask,
+						'copy.release',
+						done);
+		} else {
+			runSequence('clean.release',
+						releaseTask,
+						'copy.release',
+						done);
+		}
 	});
 };
