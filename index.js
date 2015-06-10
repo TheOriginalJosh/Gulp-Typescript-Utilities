7e4d2ccc6a1ecfabcdd70ec73a3d49a1fc835721
Josh Graber <jgraber@renovo1.com>
2015-06-10 12:55:26 -0400

    Add compile and rearrange (alphabetical)

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 var build = require('./build');
 var clean = require('./clean');
+var compile = require('./compile');
 var copy = require('./copy');
 var karma = require('./default-karma.conf');
 var lint = require('./lint');
 var requirejs = require('./requirejs');
 var resolve = require('./resolve');
-var test = require('./test');
 var teamCity = require('./teamCity');
+var test = require('./test');
 var typescript = require('./typescript');
 
 module.exports = {
 	build: build,
 	clean: clean,
+	compile: compile,
 	copy: copy,
 	karma: karma,
 	lint: lint,
 	requirejs: requirejs,
 	resolve: resolve,
-	test: test,
 	teamCity: teamCity,
+	test: test,
 	typescript: typescript,
 };
