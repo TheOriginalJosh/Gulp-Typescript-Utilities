83398f7765f955161eb45cf7dd3ba4e5964cdd37
Josh Graber <jgraber@renovo1.com>
2015-06-10 09:01:38 -0400

    Modify requirejs module to configure subtasks instead of providing a function. This lets us run tasks in sequence to get ordering of steps.

--- requirejs.js
+++ requirejs.js
@@ -1,36 +1,40 @@
-var gulp = require('gulp');
+var runSequence = require('run-sequence');
 
-var requirejs = require('requirejs');
 var typescript = require('./typescript');
 var del = require('del');
 
 var merge = require('merge2');
 
-exports.optimize = function(packageName, source, target, done) {
-	var sourceStream = gulp.src([
-		'./typings/tsd.d.ts',
-		'./' + source + '/**/*.ts',
-	]);
-	
-	var result = typescript.compile(sourceStream, true);
+exports.config = function (gulp, packageName, source, target) {
+	gulp.task('compile.release.bin', function () {
+		var sourceStream = gulp.src([
+			'./typings/tsd.d.ts',
+			'./' + source + '/**/*.ts',
+		]);
 
-	var out = merge([
-		result.js.pipe(gulp.dest('bin')),
-		result.dts.pipe(gulp.dest('./' + target)),
-	]);
-	out.on('end', function() {
+		var result = typescript.compile(sourceStream, true);
+
+		return merge([
+			result.js.pipe(gulp.dest('bin')),
+			result.dts.pipe(gulp.dest('./' + target)),
+		]);
+	});
+
+	gulp.task('compile.release.optimize', function (done) {
 		requirejs.optimize({
-		    baseUrl: 'bin',
-		    name: packageName,
-		    out: target + '/' + packageName + '.js',
-		}, function(buildResponse) {
+			baseUrl: 'bin',
+			name: packageName,
+			out: target + '/' + packageName + '.js',
+		}, function (buildResponse) {
 			console.log(buildResponse);
-		}, function(err) {
+			done();
+		}, function (err) {
 			console.log(err);
+			done();
 		});
-		
+	});
+
+	gulp.task('compile.release.clean', function (done) {
 		del('bin', done);
 	});
-	
-	return out;
-};
+}
