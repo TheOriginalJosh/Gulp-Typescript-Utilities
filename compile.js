1c826af73cb0bd1b875e74704e780d53a66c1f67
Josh Graber <jgraber@renovo1.com>
2015-07-07 10:51:17 -0400

    Remove requirejs (using internal TypeScript modules) And update to match the new TypeScript services (no path required now).

--- compile.js
+++ compile.js
@@ -1,21 +1,15 @@
 var runSequence = require('run-sequence');
 
 var typescript = require('./typescript');
-var requirejs = require('./requirejs');
 
 exports.config = function(gulp, packageName, locations) {
-	requirejs.config(gulp, packageName, locations);
-	
 	gulp.task('compile', ['compile.debug']);
 	
 	gulp.task('compile.debug', function() {
-		return typescript.compileDebug('**/*.ts', locations.source, locations.debug, gulp);
+		return typescript.compileDebug(locations.source, locations.debug, gulp);
 	});
 	
 	gulp.task('compile.release', function(done) {
-		runSequence('compile.release.bin',
-					'compile.release.optimize',
-					'compile.release.clean',
-					done);
+		return typescript.compileRelease(locations.source, locations.release, true, gulp);
 	});
 };
