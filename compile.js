b70c3f8f711a19162c62f9e30064ad747b303318
Josh Graber <jgraber@renovo1.com>
2015-07-23 12:15:45 -0400

    Debug and release can't be compiled at the same time. When doing library compile, add .min to the release output.

--- compile.js
+++ compile.js
@@ -15,10 +15,30 @@ exports.config = function(gulp, packageName, locations) {
 
 	var merge = require('merge2');
 
-	gulp.task('compile.library', function() {
-		return merge([
-			typescript.compileDebug(locations.source, locations.library, false, gulp),
-			typescript.compileRelease(locations.source, locations.library, false, gulp),
-		]);
+	gulp.task('compile.library', function(done) {
+		runSequence('compile.library.debug',
+					'compile.library.setOutputMinify',
+					'compile.library.release',
+					'compile.library.resetOutputMinify',
+					done);
 	});
+	
+	gulp.task('compile.library.debug', function() {
+		return typescript.compileDebug(locations.source, locations.library, true, gulp);
+	});
+
+	var defaultOutput = typescript.project.out;
+	
+	gulp.task('compile.library.setOutputMinify', function() {
+		var match = /(.*)(\.js)/;
+		typescript.project.out = defaultOutput.replace(match, '$1.min$2');
+	})
+	
+	gulp.task('compile.library.release', function() {
+		return typescript.compileRelease(locations.source, locations.library, false, gulp);
+	});
+	
+	gulp.task('compile.library.resetOutputMinify', function() {
+		typescript.project.out = defaultOutput;
+	})
 };
