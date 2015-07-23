06a7787536a9cae5a126f2e4fd7e76ad6ff8f939
Josh Graber <jgraber@renovo1.com>
2015-07-23 11:57:45 -0400

    The release compilation should NOT include tests while the debug compilation SHOULD.

--- compile.js
+++ compile.js
@@ -6,10 +6,10 @@ exports.config = function(gulp, packageName, locations) {
 	gulp.task('compile', ['compile.debug']);
 	
 	gulp.task('compile.debug', function() {
-		return typescript.compileDebug(locations.source, locations.debug, false, gulp);
+		return typescript.compileDebug(locations.source, locations.debug, true, gulp);
 	});
 	
 	gulp.task('compile.release', function(done) {
-		return typescript.compileRelease(locations.source, locations.release, true, gulp);
+		return typescript.compileRelease(locations.source, locations.release, false, gulp);
 	});
 };
