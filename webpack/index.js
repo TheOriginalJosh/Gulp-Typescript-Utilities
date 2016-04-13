be18f62270fd5c16c3eeb51cc538fe21ca0d04ce
Josh Graber <jgraber@renovo1.com>
2016-04-13 11:49:28 -0400

    Remove the reference to the ts webpack configuration which was removed.

--- webpack/index.js
+++ webpack/index.js
@@ -1,11 +1,9 @@
 var karma = require('./karma.config');
 var library = require('./library.config');
 var libraryMin = require('./library.min.config');
-var ts = require('./ts.config');
 
 module.exports = {
 	karma: karma,
 	library: library,
 	libraryMin: libraryMin,
-	ts: ts,
 };
